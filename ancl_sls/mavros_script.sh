7039200d6c520891f18484c760506e024149890f
Zichen Zhang <zhangzichen1105@gmail.com>
2024-11-06 22:40:01 -0700

    Update mavros_script.sh

--- ancl_sls/mavros_script.sh
+++ ancl_sls/mavros_script.sh
@@ -1,4 +1,4 @@
-cd ~/PX4_SITL_664
+cd ~/ACC-Repository
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 source /usr/share/gazebo-11/setup.bash
 source $(pwd)/build/px4_sitl_default/build_gazebo/devel/setup.bash
